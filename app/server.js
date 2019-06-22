a4145eb558a4ad3a8d575ba17a44d08607057f9c
Mohamed Gamal <mohamedelsonny5@gmail.com>
2019-06-22 18:52:54 +0200

    added upload to apollo-server

--- app/server.js
+++ app/server.js
@@ -19,7 +19,15 @@ const server = new ApolloServer({
   schema: schemaWithMiddlewares,
   context,
   introspection: true,
-  playground: true
+  playground: true,
+  uploads: {
+    // Limits here should be stricter than config for surrounding
+    // infrastructure such as Nginx so errors can be handled elegantly by
+    // graphql-upload:
+    // https://github.com/jaydenseric/graphql-upload#type-uploadoptions
+    maxFileSize: 1000000000, // 100 MB
+    maxFiles: 20
+  }
 });
 
 const app = express();
