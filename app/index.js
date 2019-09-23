27a7ff65705e79e70e7eb4b94db0e9b46882bd50
bazokhan <albaz2000eg@gmail.com>
2019-09-23 22:41:24 +0200

    configure port for heroku

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 const server = require('./server');
 
-server.listen({ port: 4000 }, () =>
+server.listen({ port: process.env.PORT || 4000 }, () =>
   console.log(
     `🚀 Server ready at http://localhost:4000/graphql , env: ${
       process.env.NODE_ENV
