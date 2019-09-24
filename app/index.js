87381781a493153ca0ff8cb0fc1d499286c03597
bazokhan <albaz2000eg@gmail.com>
2019-09-24 03:19:07 +0200

    retrying setup

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const server = require('./server');
 
 server.listen({ port: process.env.PORT || 4000 }, () =>
   console.log(
-    `🚀 Server ready at http://localhost:4000/graphql , env: ${
+    `🚀 Server ready at http://localhost:${process.env.PORT || 'port'}/graphql , env: ${
       process.env.NODE_ENV
     }`
   )
